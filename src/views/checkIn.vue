d496fce39e160235971a0a1e598604d0c2c08bcd
Cyclone <15927488117@163.com>
2024-06-25 21:24:38 +0800

    fix:Re-check in

--- src/views/checkIn.vue
+++ src/views/checkIn.vue
@@ -241,8 +241,7 @@
           <span class="finished">{{ `- ${challengeInfo?.supplementaryEnergy}` }}</span>
           <v-icon color="#FFF100" :size="16" icon="mdi-lightning-bolt"></v-icon>
         </div>
-        <v-btn class="re_check_in" @click="handleReCheckin()" :loading="reCheckinLoading"
-          :disabled="Number(challengeInfo?.supplementaryEnergy) >= Number(userInfo?.energyAmount)">
+        <v-btn class="re_check_in" @click="handleReCheckin()" :loading="reCheckinLoading">
           <span class="finished">Re-Check In</span>
         </v-btn>
       </div>
