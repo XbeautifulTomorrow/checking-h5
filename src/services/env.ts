a455a7b9077c47c61f634d261ced9bed8581ab4d
Cyclone <15927488117@163.com>
2024-06-26 19:15:27 +0800

    fix:config

--- src/services/env.ts
+++ src/services/env.ts
@@ -2,12 +2,15 @@ const config = {
   dev: {
     api: "http://222.214.219.202:4401/",
   },
-  prod: {
+  test: {
     api: "/"
+  },
+  prod: {
+    api: "https://api.gmking.io/"
   },
 };
 
 export default {
   ENV: import.meta.env.NODE_ENV,
-  ...config[import.meta.env.MODE as keyof typeof config],
+  ...config[import.meta.env.VITE_SERVER_CONFIG as keyof typeof config],
 };
